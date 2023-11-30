1fcf979d78fee3db8bf8d73abcbfe878479d7b90
matygullinruslan <matygullinruslan@gmail.com>
2023-11-30 20:19:30 +0300

    Ошибка >       if 6 < time < 22: E       TypeError: '<' not supported between instances of 'int' and 'type'

--- test_qa_guru.py
+++ test_qa_guru.py
@@ -8,7 +8,7 @@ def test_dark_theme_by_time():
     """
     current_time = time(hour=23)
     # TODO переключите темную тему в зависимости от времени суток (с 22 до 6 часов утра - ночь)
-    if 6 < time < 22:
+    if 6 < current_time < 22:
         is_dark_theme = False
     else:
         is_dark_theme = True
