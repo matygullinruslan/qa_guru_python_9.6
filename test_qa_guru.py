ef1c89434cf0a28d52b7563203548d7b76cab2c4
matygullinruslan <matygullinruslan@gmail.com>
2023-12-05 00:25:44 +0300

    test_qa

--- test_qa_guru.py
+++ test_qa_guru.py
@@ -6,13 +6,17 @@ def test_dark_theme_by_time():
     Протестируйте правильность переключения темной темы на сайте в зависимости от времени
     :param dark_theme:
     """
-    current_time = time(hour=23)
-    # TODO переключите темную тему в зависимости от времени суток (с 22 до 6 часов утра - ночь)
-    if 6 < current_time < 22:
-        is_dark_theme = False
-    else:
-        is_dark_theme = True
-    assert is_dark_theme is True
+    is_dark_theme = None
+
+
+current_time = time(hour=23)
+
+if current_time.hour > 22 or current_time.hour < 6:
+    is_dark_theme = True
+# TODO переключите темную тему в зависимости от времени суток (с 22 до 6 часов утра - ночь)
+else:
+    False
+assert is_dark_theme is True
 
 
 def test_dark_theme_by_time_and_user_choice():
@@ -23,13 +27,17 @@ def test_dark_theme_by_time_and_user_choice():
     dark_theme_enabled_by_user = False - Темная тема выключена
     dark_theme_enabled_by_user = None - Пользователь не сделал выбор (используется переключение по времени системы)
     """
-    current_time = time(hour=16)
+    current_time = time(hour=23)
     dark_theme_enabled_by_user = True
     # TODO переключите темную тему в зависимости от времени суток,
     #  но учтите что темная тема может быть включена вручную
 
-    is_dark_theme = None
-    assert is_dark_theme is True
+
+if current_time.hour > 22 or current_time.hour < 6 or dark_theme_enabled_by_user:
+    is_dark_theme = True
+else:
+    is_dark_theme = False
+assert is_dark_theme is True
 
 
 def test_find_suitable_user():
@@ -45,15 +53,24 @@ def test_find_suitable_user():
     ]
 
     # TODO найдите пользователя с именем "Olga"
-    suitable_users = None
-    assert suitable_users == {"name": "Olga", "age": 45}
 
-    # TODO найдите всех пользователей младше 20 лет
-    suitable_users = None
-    assert suitable_users == [
-        {"name": "Stanislav", "age": 15},
-        {"name": "Maria", "age": 18},
-    ]
+
+suitable_user = None
+for user in users:
+    if user['name'] == 'Olga':
+        suitable_user = user
+assert suitable_user == {"name": "Olga", "age": 45}
+
+# TODO найдите всех пользователей младше 20 лет
+
+suitable_user = None
+for user in users:
+    if user['age'] < 20:
+        suitable_user = user
+assert suitable_user == [
+    {"name": "Stanislav", "age": 15},
+    {"name": "Maria", "age": 18},
+]
 
 
 # Сделайте функцию, которая будет печатать
@@ -73,16 +90,23 @@ def test_readable_function():
     find_registration_button_on_login_page(page_url="https://companyname.com/login", button_text="Register")
 
 
+def readable_function(func, *args, **kwargs):
+    func_name = func.__name__.replace('_', ' ').title()
+
+    args_names = ", ".join([*args, *kwargs.values()])
+    return f'{func_name} {{args_names}}'
+
+
 def open_browser(browser_name):
-    actual_result = None
+    actual_result = readable_function(open_browser, browser_name)
     assert actual_result == "Open Browser [Chrome]"
 
 
 def go_to_companyname_homepage(page_url):
-    actual_result = None
+    actual_result = readable_function(go_to_companyname_homepage, page_url)
     assert actual_result == "Go To Companyname Homepage [https://companyname.com]"
 
 
 def find_registration_button_on_login_page(page_url, button_text):
-    actual_result = None
+    actual_result = readable_function(find_registration_button_on_login_page, page_url, button_text)
     assert actual_result == "Find Registration Button On Login Page [https://companyname.com/login, Register]"
